8b3e5b398af89a02cbbb22ae2659f7a0b6f32178
joaquintejero92 <joaquintejeroca@gmail.com>
2023-06-26 17:31:59 +0200

    Create director.sql

---
+++ utils/queries/director.sql
@@ -0,0 +1,7 @@
+CREATE TABLE directores (
+	id int NOT NULL AUTO_INCREMENT,
+    nombre varchar(255) NOT NULL,
+    apellidos varchar(255) NOT NULL,
+    fecha_nacimiento DATE,
+    nacionalidad varchar(255),
+);
