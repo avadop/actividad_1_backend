9717869be12b7a4e6c3a9e8567d29a93adb684fa
joaquintejero92 <joaquintejeroca@gmail.com>
2023-06-26 17:48:17 +0200

    Update director.sql

--- utils/queries/director.sql
+++ utils/queries/director.sql
@@ -3,7 +3,7 @@ CREATE TABLE directores (
     nombre varchar(255) NOT NULL,
     apellidos varchar(255) NOT NULL,
     fecha_nacimiento DATE,
-    nacionalidad varchar(255),
+    nacionalidad varchar(255)
 );
 
 -- inserts into directores
