573f2c57f878f43583f61d6481843604746c7258
joaquintejero92 <joaquintejeroca@gmail.com>
2023-06-26 17:42:55 +0200

    Update director.sql

--- utils/queries/director.sql
+++ utils/queries/director.sql
@@ -5,3 +5,15 @@ CREATE TABLE directores (
     fecha_nacimiento DATE,
     nacionalidad varchar(255),
 );
+
+-- inserts into directores
+
+INSERT INTO directores (nombre, apellidos, fecha_nacimiento, nacionalidad)
+VALUES ('Woody','Allen','1935-11-30','Estados Unidos');
+
+INSERT INTO directores (nombre, apellidos, fecha_nacimiento, nacionalidad)
+VALUES ('James','Cameron','1954-08-16','Estados Unidos');
+
+INSERT INTO directores (nombre, apellidos, fecha_nacimiento, nacionalidad)
+VALUES ('Pedro','Almodovar','1949-09-25','España');
+
