ebc030ac0b1e5c5454ed4bd9d7733a773cd757ae
pavazfer <vazquezfernandezpablo@gmail.com>
2023-06-27 15:39:26 +0200

    Modificacion series.sql

--- utils/queries/series.sql
+++ utils/queries/series.sql
@@ -1,12 +1,35 @@
 --create table series
-CREATE TABLE `actividad1_backend`.`series` (`id` INT NOT NULL AUTO_INCREMENT , `title` VARCHAR(256) NOT NULL , `platform` INT NOT NULL , `director` INT NOT NULL , `actors` INT NOT NULL , `audio_language` INT NOT NULL , `subtitles_language` INT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+CREATE TABLE `actividad1_backend`.`series` (
+	`id` INT NOT NULL AUTO_INCREMENT , 
+	`title` VARCHAR(256) NOT NULL , 
+	`platform` INT NOT NULL , 
+	`director` INT NOT NULL , 
+	`audio_language` INT NOT NULL , 
+	`subtitles_language` INT NOT NULL , 
+	PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
 
 -- alter table 
-ALTER TABLE `series` ADD FOREIGN KEY (`platform`) REFERENCES `platforms`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE; 
-ALTER TABLE `series` ADD FOREIGN KEY (`director`) REFERENCES `directors`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE; 
-ALTER TABLE `series` ADD FOREIGN KEY (`audio_language`) REFERENCES `languages`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
-ALTER TABLE `series` ADD FOREIGN KEY (`subtitles_language`) REFERENCES `languages`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE; 
-ALTER TABLE `series` ADD FOREIGN KEY (`actors`) REFERENCES `actors`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `series` 
+	ADD FOREIGN KEY (`platform`) 
+	REFERENCES `platforms`(`id`) 
+	ON DELETE RESTRICT ON UPDATE CASCADE; 
+
+ALTER TABLE `series` 
+	ADD FOREIGN KEY (`director`) 
+	REFERENCES `directors`(`id`) 
+	ON DELETE RESTRICT ON UPDATE CASCADE; 
+
+ALTER TABLE `series` 
+	ADD FOREIGN KEY (`audio_language`) 
+	REFERENCES `languages`(`id`) 
+	ON DELETE RESTRICT ON UPDATE CASCADE;
+	
+ALTER TABLE `series` 
+	ADD FOREIGN KEY (`subtitles_language`) 
+	REFERENCES `languages`(`id`) 
+	ON DELETE RESTRICT ON UPDATE CASCADE; 
 
 -- insert into series
-INSERT INTO `series`(`title`, `platform`, `director`, `actors`, `audio_language`, `subtitles_language`) VALUES ('Título serie',1,1,1,1,2);
+INSERT INTO `series`(`title`, `platform`, `director`,  `audio_language`, `subtitles_language`) 
+VALUES ('Título serie',1,1,1,2);
